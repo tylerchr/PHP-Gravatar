6617b01ac07f39512015d6d5ad4461f52f26c03c
Tyler Christensen <tyler@jauntyserv.localdomain>
2010-01-07 05:20:26 -0700

    Initial commit of updated Gravatar class

---
+++ sample.php
@@ -0,0 +1,25 @@
+<?php
+	
+	// Get an email address from the sample.php?email= parameter
+	$email = $_GET['email'];
+	require('Gravatar.class.php');
+	$grav = new Gravatar($email);
+	$grav->size = 128;
+
+?>
+
+<html>
+	<head>
+		<title>Gravatar for <?php echo $grav->email; ?></title>
+	</head>
+	<body>
+		<?php
+			if ($grav->gravatarExists()) {
+				echo '<img src="' . $grav . '" alt="" />';
+			} else {
+				echo 'No Gravatar exists for this email address';	
+			}
+		?>
+		
+	</body>
+</html>
